a154f68c5e1fc8c77638b4f405abd0c5859062e1
liyuanhua123 <13418764759@163.com>
2019-03-19 17:01:00 +0800

    Initial commit

---
+++ scripts/Test02.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import allure
+
+import allure
+import pytest
+
+
+class Test02:
+
+    @allure.step("01yi")
+    @allure.severity("critical")
+    def test001(self):
+      print("我是被执行")
+
+    allure.attach("02bu","")
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
+    def test002(self):
+        print("我被更新")
